748d7c72298b5d3a25fd86b3167118f5bbdc832f
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2020-12-09 22:25:12 +0800

    Implement sending of transaction.

---
+++ api/helpers/TransactionHelper.js
@@ -0,0 +1,37 @@
+
+module.exports = {
+    buildTransaction: async(encodedABI) => {
+        const Web3 = require('web3');
+
+        const web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER_URL));
+        const Tx = require('ethereumjs-tx');
+
+        const txParams = {
+            nonce: await web3.eth.getTransactionCount(process.env.ETH_ADDRESS),
+            value: 0,
+            gasLimit: web3.utils.toHex(10000000),
+            gasPrice: web3.utils.toHex(0),
+            data: encodedABI,
+        };
+
+        // Initialize a new Transaction
+        const tx = new Tx(txParams);
+
+        // Sign the Transaction with sender's private key
+        tx.sign(Buffer.from( // convert string to Buffer
+            process.env.ETH_PRIVATE_KEY.substring(2), // remove 0x
+            'hex',
+        ));
+
+        // Returns the rlp (Recursive Length Prefix) encoding of the transaction, https://eth.wiki/en/fundamentals/rlp
+        const serializedTx = tx.serialize();
+
+        // Convert buffer (array of 8-bit unsigned integers) to string
+        const rawTx = '0x' + serializedTx.toString('hex'); // 0x + 247970ea6031894B5Da5B32c6E7BdDca223f6735
+        
+        // Send signed transaction to the chain
+        const transaction = await web3.eth.sendSignedTransaction(rawTx);
+        
+        return transaction.transactionHash;
+    }
+}
--- api/routes/v1/suppliers/index.js
+++ api/routes/v1/suppliers/index.js
@@ -3,11 +3,16 @@ module.exports = {
     post: async (req,res,next) => {
         const Accounts = require('web3-eth-accounts');
         const accounts = new Accounts(process.env.WEB3_PROVIDER_URL);
-        
+        const Web3 = require('web3');
+        const Tx = require('ethereumjs-tx');
         const bcrypt = require('bcrypt');
+        const TransactionHelper = require('../../../helpers/TransactionHelper');
 
         try{
            
+            // Connect to chain
+            const web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER_URL));
+            const smartContract = new web3.eth.Contract(contractABI,process.env.ETH_ADDRESS);
 
             const db = firebase.firestore()
             
@@ -18,7 +23,27 @@ module.exports = {
             
             let encodedPrivateKey = bcrypt.hashSync(privateKey,10);
 
+            // Check if address already exists
+            /*const suppliersCollection = db.collection('suppliers');
+            const addressQuery = await suppliersCollection.where('address', '==', address).get();
+            
+            if(addressQuery == null){
+                console.log('Not found');
+            }
+            */
+
+            let registerSupplierEncodedABI = smartContract
+                .methods
+                .registerUser(address,1)
+                .encodeABI();
+
+            let transactionHash = await TransactionHelper.buildTransaction(
+                registerSupplierEncodedABI
+            )    
+
             let doc = await db.collection('suppliers').add({
+                name: req.body.name,
+                businessAddress: req.body.businessAddress,
                 address: address,
                 private_key: encodedPrivateKey
             });
@@ -30,6 +55,7 @@ module.exports = {
             return
         }
         catch(err){
+            console.log(err)
             res.send(500,{
                 message: 'Account creation failed',
                 error: err
