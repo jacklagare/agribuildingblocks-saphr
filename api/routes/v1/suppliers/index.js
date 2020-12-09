5d9e81f5ad3f455cba3ff426fda1971cef92d9ba
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2020-12-09 18:37:20 +0800

    Implement storing of address and private key to Firebase.

--- api/routes/v1/suppliers/index.js
+++ api/routes/v1/suppliers/index.js
@@ -4,28 +4,43 @@ module.exports = {
         const Accounts = require('web3-eth-accounts');
         const accounts = new Accounts(process.env.WEB3_PROVIDER_URL);
         const admin = require('firebase-admin');
+        const bcrypt = require('bcrypt');
 
         const serviceAccount = require('../../../config/serviceAccountKey.json');
 
-        admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount)
-        });
+        try{
+            admin.initializeApp({
+                credential: admin.credential.cert(serviceAccount)
+            });
 
-        const db = admin.firestore()
-           
-        
-        let newAccount = accounts.create();
-        let address = newAccount.address;
-        //let privateKey = newAccount.
-        console.log(newAccount);
-        const docRef = db.collection('blockchain-capstone').doc('supplier-accounts');
+            const db = admin.firestore()
+            
+            let newAccount = accounts.create();
+            let address = newAccount.address;
+            
+            let privateKey = newAccount.privateKey.substring(2);
+            
+            let encodedPrivateKey = bcrypt.hashSync(privateKey,10);
 
-        await docRef.set({
-            address: 'Ada',
-            private_key: 'Lovelace',
-        });
+            const docRef = db.collection('blockchain-capstone').doc('supplier-accounts');
 
-        res.send(200,'Hello');
-        return
+            await docRef.set({
+                address: address,
+                private_key: encodedPrivateKey,
+            });
+
+            res.send(200,{
+                message: 'Eth account successfully created for supplier.',
+                address: address
+            });
+            return
+        }
+        catch(err){
+            res.send(500,{
+                message: 'Account creation failed',
+                error: err
+            });
+            return
+        }
     },
 }
