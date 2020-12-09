cf161ba9bfd85cf60c562b26ace671e021946c5c
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2020-12-09 18:25:56 +0800

    Implement creation of user accounts.

--- api/routes/v1/suppliers/index.js
+++ api/routes/v1/suppliers/index.js
@@ -1,6 +1,30 @@
 module.exports = {
 
     post: async (req,res,next) => {
+        const Accounts = require('web3-eth-accounts');
+        const accounts = new Accounts(process.env.WEB3_PROVIDER_URL);
+        const admin = require('firebase-admin');
+
+        const serviceAccount = require('../../../config/serviceAccountKey.json');
+
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount)
+        });
+
+        const db = admin.firestore()
+           
+        
+        let newAccount = accounts.create();
+        let address = newAccount.address;
+        //let privateKey = newAccount.
+        console.log(newAccount);
+        const docRef = db.collection('blockchain-capstone').doc('supplier-accounts');
+
+        await docRef.set({
+            address: 'Ada',
+            private_key: 'Lovelace',
+        });
+
         res.send(200,'Hello');
         return
     },
