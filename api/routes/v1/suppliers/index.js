ef3cdc6fd0fec69624162de0ad3250cb24626c53
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2020-12-12 19:50:50 +0800

    Implement checking of existing suppliers.

--- api/routes/v1/suppliers/index.js
+++ api/routes/v1/suppliers/index.js
@@ -9,7 +9,6 @@ module.exports = {
         const TransactionHelper = require('../../../helpers/TransactionHelper');
 
         try{
-           
             // Connect to chain
             const web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER_URL));
             const smartContract = new web3.eth.Contract(contractABI,process.env.ETH_ADDRESS);
@@ -23,50 +22,75 @@ module.exports = {
             
             let encodedPrivateKey = bcrypt.hashSync(privateKey,10);
             
-            // Check if address already exists
-            /*const suppliersCollection = db.collection('suppliers');
-            const addressQuery = await suppliersCollection.where('address', '==', address).get();
+            console.log("Check for existing suppliers...");
+
+            let docRef = await db.collection("suppliers").select("name").where("name", "==",  req.body.name.trim());
             
-            if(addressQuery == null){
-                console.log('Not found');
-            }
-            */
+            let uniqueSupplier = true;
 
-            let registerSupplierEncodedABI = smartContract
-                .methods
-                .registerUser(address,1)
-                .encodeABI();
-
-            let transactionHash = await TransactionHelper.buildTransaction(
-                process.env.CONTRACT_OWNER_ADDRESS,
-                process.env.CONTRACT_OWNER_PRIVATE_KEY,
-                registerSupplierEncodedABI
-            )    
-            
-            let transactionReceipt = await web3.eth.getTransaction(transactionHash);
-
-            if(transactionReceipt.status){
-                let doc = await db.collection('suppliers').add({
-                    name: req.body.name,
-                    businessAddress: req.body.businessAddress,
-                    address: address,
-                    private_key: encodedPrivateKey
-                });
-
-                res.send(200,{
-                    message: 'Account successfully created for supplier.',
-                    address: address,
-                    privateKey: privateKey
-                });
-                return
-            }
-            else{
+            await docRef.get().then(function(doc) {
+                if (!doc.empty) {
+                    uniqueSupplier = false;
+                } 
+            }).catch(function(error) {
                 console.log(err)
                 res.send(500,{
-                message: 'Account creation failed',
-                error: err
+                    message: 'Account creation failed',
+                    error: error
+                });
+                return
             });
-            return
+
+            if(uniqueSupplier){
+                let registerSupplierEncodedABI = smartContract
+                    .methods
+                    .registerUser(address,1)
+                    .encodeABI();
+
+                console.log('Sending transaction to blockchain...');
+                let transactionHash = await TransactionHelper.buildTransaction(
+                    process.env.CONTRACT_OWNER_ADDRESS,
+                    process.env.CONTRACT_OWNER_PRIVATE_KEY,
+                    registerSupplierEncodedABI
+                )    
+                console.log('Done sending transaction to blockchain...');
+                
+                console.log('Checking status of transaction.');
+                
+                let transactionReceipt = await web3.eth.getTransactionReceipt(transactionHash);
+        
+                console.log('Done checking status of transaction.');
+
+                if(transactionReceipt.status){
+                    
+                        let doc = db.collection('suppliers').add({
+                            name: req.body.name,
+                            businessAddress: req.body.businessAddress,
+                            address: address,
+                            private_key: encodedPrivateKey
+                        });
+
+                        res.send(200,{
+                            message: 'Account successfully created for supplier.',
+                            address: address,
+                            privateKey: privateKey
+                        });
+                        return
+                }
+                else{
+                    res.send(500,{
+                        message: 'Account creation failed',
+                        error: 'Blockchain transaction failed.'
+                    });
+                    return
+                }
+            }
+            else{
+                res.send(500,{
+                    message: 'Account creation failed',
+                    error: 'Account being created already exists.'
+                });
+                return
             }
         }
         catch(err){
