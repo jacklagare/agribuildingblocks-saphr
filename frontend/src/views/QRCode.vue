1e7e68282024408df22babd583c6e03a033b36c9
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2020-12-12 14:54:11 +0800

    Implement QR code generation.

---
+++ frontend/src/views/QRCode.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <loading :active.sync="isLoading" 
+        :can-cancel="true" 
+        :is-full-page="fullPage">
+        </loading>    
+        <br/><br/>
+        <qrcode-vue :value="value" :size="size" level="H"></qrcode-vue>
+        <br/><br/>
+        <v-form>
+            <v-container>
+                <v-text-field
+                v-model="batchId"
+                counter="25"
+                label="Batch ID"
+                >
+                </v-text-field>
+            </v-container>
+        </v-form>
+        <v-btn
+            color="primary"
+            depressed
+            elevation="7"
+            outlined
+            text
+            @click="generateQrCode"
+        >
+        Generate QR Code
+    </v-btn>
+    </div>
+</template>
+<script>
+
+import QrcodeVue from 'qrcode.vue'
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+
+export default {
+    data() {
+        return {
+            isLoading: false,
+            fullPage: true,
+            value: "",
+            size: 300,
+            batchId: "",
+        }
+    },
+
+    methods: {
+        generateQrCode(){
+            this.isLoading = true;
+            this.value = '';
+            this.value = 'http://localhost:3000/check-meat-product/'+this.batchId
+            this.isLoading = false;
+        }
+    },
+
+    components: {
+        Loading,
+        QrcodeVue,
+    },
+}
+</script>
