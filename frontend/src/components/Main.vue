4962024ebecbbc2c948cc53f1f0bca43ecfae1d1
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2020-12-12 15:26:32 +0800

    Clean up code.

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -2,12 +2,14 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
+        <!--
         <v-img
           :src="require('../assets/logo.svg')"
           class="my-3"
           contain
           height="200"
         />
+      -->
       </v-col>
 
       <v-col class="mb-4">
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">    
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main msg=""/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Main from '@/components/Main.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
