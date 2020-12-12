501522ba90ea08e80841adffd386293cc5d9e716
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2020-12-12 20:26:45 +0800

    Implement better error handling.

--- api/routes/v1/laboratories/index.js
+++ api/routes/v1/laboratories/index.js
@@ -23,39 +23,76 @@ module.exports = {
             
             let encodedPrivateKey = bcrypt.hashSync(privateKey,10);
             
-            // Check if address already exists
-            /*const suppliersCollection = db.collection('suppliers');
-            const addressQuery = await suppliersCollection.where('address', '==', address).get();
-            
-            if(addressQuery == null){
-                console.log('Not found');
-            }
-            */
+            console.log("Check for existing inspectors...");
 
-            let registerLaboratoryEncodedABI = smartContract
+            let docRef = await db.collection("laboratories").select("name").where("name", "==",  req.body.name.trim());
+            
+            let uniqueLaboratory = true;
+
+            await docRef.get().then(function(doc) {
+                if (!doc.empty) {
+                    uniqueLaboratory = false;
+                } 
+            }).catch(function(error) {
+                console.log(err)
+                res.send(500,{
+                    message: 'Account creation failed',
+                    error: error
+                });
+                return
+            });
+
+            if(uniqueLaboratory){
+                let registerLaboratoryEncodedABI = smartContract
                 .methods
                 .registerUser(address,3)
                 .encodeABI();
 
-            let transactionHash = await TransactionHelper.buildTransaction(
-                process.env.CONTRACT_OWNER_ADDRESS,
-                process.env.CONTRACT_OWNER_PRIVATE_KEY,
-                registerLaboratoryEncodedABI
-            )    
+                console.log('Sending transaction to blockchain...');
+                let transactionHash = await TransactionHelper.buildTransaction(
+                    process.env.CONTRACT_OWNER_ADDRESS,
+                    process.env.CONTRACT_OWNER_PRIVATE_KEY,
+                    registerLaboratoryEncodedABI
+                )    
+                console.log('Done sending transaction to blockchain...');
+                
+                console.log('Checking status of transaction.');
+                
+                let transactionReceipt = await web3.eth.getTransactionReceipt(transactionHash);
+        
+                console.log('Done checking status of transaction.');
 
-            let doc = await db.collection('laboratories').add({
-                name: req.body.name,
-                businessAddress: req.body.businessAddress,
-                address: address,
-                private_key: encodedPrivateKey
-            });
+                if(transactionReceipt.status){
+                    let doc = await db.collection('laboratories').add({
+                        name: req.body.name,
+                        businessAddress: req.body.businessAddress,
+                        address: address,
+                        private_key: encodedPrivateKey
+                    });
 
-            res.send(200,{
-                message: 'Account successfully created for laboratory.',
-                address: address,
-                privateKey: privateKey
-            });
-            return
+                    res.send(200,{
+                        message: 'Account successfully created for laboratory.',
+                        address: address,
+                        privateKey: privateKey
+                    });
+                    return
+                }
+                else{
+                    res.send(500,{
+                        message: 'Account creation failed',
+                        error: 'Blockchain transaction failed.'
+                    });
+                    return
+                }
+            }
+            else{
+                res.send(500,{
+                    message: 'Account creation failed',
+                    error: 'Account being created already exists.'
+                });
+                return
+            }
+            
         }
         catch(err){
             console.log(err)
