4229c422807cd86f0103b126a8f6598b68dadd26
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2020-12-14 17:00:23 +0800

    Add instructions on how to use the SMS service.

--- frontend/src/views/QRCode.vue
+++ frontend/src/views/QRCode.vue
@@ -15,8 +15,55 @@
                     <qrcode-vue :value="value" :size="size" level="H"></qrcode-vue>
                     <br/>
                     <p>To check information about the meat product, you may scan the QR code or 
-                    visit the link indicated below:</p>
-                    <h3><v-icon>mdi-link</v-icon>&nbsp;&nbsp;<v-chip><a :href="generatedUrl" target="_new">{{generatedUrl}}</a></v-chip></h3>
+                    click the link below:</p>
+                    <v-btn color="primary" :href="generatedUrl" target="_blank"><v-icon>mdi-magnify</v-icon> Check</v-btn>
+                    <br/><br/>
+                    <p>Another option is to use SMS. Click below for the instructions.</p>
+                    <v-dialog
+                        v-model="dialog"
+                        width="500"
+                    >
+                        <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                            color="primary"
+                            dark
+                            v-bind="attrs"
+                            v-on="on"
+                        >
+                            <v-icon>mdi-message-processing</v-icon> SMS
+                        </v-btn>
+                        </template>
+                
+                        <v-card>
+                        <v-card-title class="headline grey lighten-2">
+                            SMS Meat Product Inquiry
+                        </v-card-title>
+                            <v-card-text>
+                                <br/>
+                                <p><v-icon>mdi-dialpad</v-icon> Globe/TM: <code>21582903</code> / Other Networks: <code>225652903</code></p>
+                                
+                                <ol>
+                                    <li>Register by sending <code>INFO</code> to any of the indicated numbers above.</li>
+                                    <li>Reply with <code>YES</code>.</li>
+                                    <li>Once registered, you may now use the SMS service. Send <b>ABB {space} {batchId}</b> to any of the indicated numbers above. For example, <code>ABB 2e039ebf-b6eb-44c7-bb52-e4d3b1c69721</code>.</li>
+                                </ol>
+                            </v-card-text>
+                
+                        <v-divider></v-divider>
+                
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn
+                            color="primary"
+                            text
+                            @click="dialog = false"
+                            >
+                            OK
+                            </v-btn>
+                        </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+                    
                 </v-col>
                 <v-col md="6">
                     <v-form>
@@ -59,6 +106,7 @@ export default {
         return {
             isLoading: false,
             fullPage: true,
+            dialog: false,
             value: "",
             size: 300,
             batchId: "",
@@ -85,7 +133,7 @@ export default {
 
 <style scoped>
     .qrcode{
-        margin-top: -20%;
+        margin-top: -10%;
         text-align: center;
     }
 </style>
